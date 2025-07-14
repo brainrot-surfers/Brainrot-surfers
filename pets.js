0e1b2fcc92cd38a1f09a62fba037eef9caa13075
brainrot-surfers <jadatallah02@gmail.com>
2025-07-14 18:44:24 +0300

    Create pets.js

---
+++ pets.js
@@ -0,0 +1,32 @@
+const pets = [
+  {
+    name: "Br Br Patapim",
+    rarity: "Uncommon",
+    image: "images/br_br_patapim.png"
+  },
+  {
+    name: "Tripi Tropi",
+    rarity: "Rare",
+    image: "images/tripi_tropi.png"
+  },
+  {
+    name: "Chimpanzee Bananine",
+    rarity: "Rare",
+    image: "images/chimpanzee_bananine.png"
+  },
+  {
+    name: "Ballerina Cappuccino",
+    rarity: "Epic",
+    image: "images/ballerina_cappuccino.png"
+  },
+  {
+    name: "Tralalero Tralala",
+    rarity: "Legendary",
+    image: "images/tralalero_tralala.png"
+  },
+  {
+    name: "Tung Tung Sahur",
+    rarity: "Mythical",
+    image: "images/tung_tung_sahur.png"
+  }
+];
